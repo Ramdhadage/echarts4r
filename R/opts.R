692b1323e3edc024b137830d07feb818ab0eafbd
Aaron Gorman <48027133+aarong1@users.noreply.github.com>
2025-07-21 21:56:54 +0100

    Update opts.R
    fix: small typo

--- R/opts.R
+++ R/opts.R
@@ -1055,7 +1055,7 @@ e_arrange <- function(..., rows = NULL, cols = NULL, width = "xs", title = NULL)
             rel="stylesheet",
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css",
             integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr",
-            crossorigin="anonymous">
+            crossorigin="anonymous"
           )
         ),
         htmltools::h3(title),
